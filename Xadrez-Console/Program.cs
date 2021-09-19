da64fe88820af7656e7344d75e7deaf549eb8896
Ingrith Reis <ingrith.reis@hotmail.com>
2021-09-19 19:24:52 -0300

    Adicionar arquivos de projeto.

---
+++ Xadrez-Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using tabuleiro;
+
+namespace Xadrez_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p;
+
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
+        }
+    }
+}
