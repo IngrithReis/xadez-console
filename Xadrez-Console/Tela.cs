eefad02b5b97fd7afb1f2f1e310de846237ad147
Ingrith Reis <ingrith.reis@hotmail.com>
2021-09-19 20:36:32 -0300

    Começando a implementar método que imprime tabuleiro

---
+++ Xadrez-Console/Tela.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using tabuleiro;
+
+namespace Xadrez_Console
+{
+    class Tela
+    { public static void ImprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int l = 0; l < tab.Linhas; l++) // varre primeiro cada coluna de acordo com a quantidade de linhas. coluna, coluna, coluna...
+            {   
+                for(int c = 0; c< tab.Colunas; c++)
+                {
+                    if (tab.PecaPosicao(l, c) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write(tab.PecaPosicao(l, c) + " ");
+                    }
+                   
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
